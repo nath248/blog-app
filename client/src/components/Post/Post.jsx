1c5d744cdbd1dc3cf4bd1c2fbd92042f635dff59
natherrera248 <nmherrera20@outlook.com>
2021-12-20 15:25:44 -0500

    add css flexbox for posts

--- client/src/components/Post/Post.jsx
+++ client/src/components/Post/Post.jsx
@@ -2,11 +2,15 @@ import "./Post.css"
 
 function Post(props) {
   return (
-    <div>
-      <img src={props.profilePic} alt={props._id} />
-      <h3>{props.name}</h3>
-      <p>{props.status}</p>
-      <p>{props.comment}</p>
+    <div className="post-box-info">
+      <div className="post-name">
+        <img src={props.profilePic} alt={props._id} />
+        <h3>{props.name}</h3>
+      </div>
+      <div className="post-description">
+        <p>{props.status}</p>
+        <p>{props.comment}</p>
+      </div>
     </div>
   )
 }
